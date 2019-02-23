a9ec0e0f96d344732e232cfe137e0f557b4c5ebc
Sygeman <sygeman@gmail.com>
2019-02-23 17:51:07 +0300

    helpers Permisson wip

--- helpers/Permission.tsx
+++ helpers/Permission.tsx
@@ -2,24 +2,42 @@ import gql from 'graphql-tag';
 import { FC } from 'react';
 import { Query } from 'react-apollo';
 
-const CHECK_PERMISSION = gql`
-  query checkPermission($name: String!, $contextId: ID) {
-    checkPermission(name: $name, contextId: $contextId)
+const GET_PERMISSIONS = gql`
+  query getPermissions {
+    getPermissions
   }
 `;
 
+type PermissionType =
+  | 'CREATE_COMMENT'
+  | 'DELETE_COMMENT'
+  | 'CREATE_CLIP'
+  | 'DELETE_CLIP'
+  | 'SET_CLIP_REACTION'
+  | 'GET_LIKES'
+  | 'GET_FOLLOWS'
+  | 'GET_WALLETS';
+
 interface IProps {
-  name: string;
-  contextId?: string;
   children: any;
+  name?: PermissionType;
+  contextId?: string;
+  invert?: boolean;
 }
 
-export const Permission: FC<IProps> = ({ name, contextId, children }) => (
-  <Query query={CHECK_PERMISSION} variables={{ name, contextId }}>
+export const Permission: FC<IProps> = ({ name, invert, children }) => (
+  <Query query={GET_PERMISSIONS} ssr={false}>
     {({ data }) => {
-      const allow = data && data.checkPermission;
+      const allows = data && data.getPermissions ? data.getPermissions : [];
+      let allow = allows.findIndex(p => p === name) >= 0;
 
-      console.log(name, contextId, allow);
+      if (!name && allows.length > 0) {
+        allow = true;
+      }
+
+      if (invert) {
+        allow = !allow;
+      }
 
       if (typeof children !== 'function') {
         return allow ? children : null;
