a76c4d264c6792aa8a72e9c8b84672d42bdb0812
Sygeman <sygeman@gmail.com>
2019-02-23 17:49:42 +0300

    Clip Provider

--- providers/Clip.tsx
+++ providers/Clip.tsx
@@ -1,7 +1,6 @@
 import gql from 'graphql-tag';
 import { Component, FC } from 'react';
 import { Query } from 'react-apollo';
-import { getAccessToken } from '../lib/auth';
 
 export const GET_CLIP = gql`
   query getClip($where: ClipWhereUniqueInput!) {
@@ -12,6 +11,9 @@ export const GET_CLIP = gql`
       spoiler
       clipId
       cover
+      likes
+      dislikes
+      score
       createdAt
       channel {
         id
@@ -25,43 +27,32 @@ export const GET_CLIP = gql`
   }
 `;
 
-const POST_REMOVED = gql`
-  subscription postRemoved($id: ID!) {
-    postRemoved(id: $id)
-  }
-`;
-
-const POST_COMMENT_COUNT_CHANGED = gql`
-  subscription postCommentCountChanged($id: ID!) {
-    postCommentCountChanged(id: $id)
-  }
-`;
-
-const POST_REACTION_CHANGED = gql`
-  subscription postReactionChanged($id: ID!) {
-    postReactionChanged(id: $id) {
-      likes
-      dislikes
-      rating
-      reaction
-      userId
+const CLIP_UPDATED = gql`
+  subscription clipUpdated($where: ClipSubscriptionWhereInput!) {
+    clip(where: $where) {
+      node {
+        title
+        nfws
+        spoiler
+        clipId
+        cover
+        likes
+        dislikes
+        score
+      }
     }
   }
 `;
 
 interface IPropsInner {
   clip: any;
-  // subscribePostRemoved: () => void;
-  // subscribePostCommentCountChanged: () => void;
-  // subscribePostReactionChanged: () => void;
+  clipUpdated: () => void;
   children: any;
 }
 
 class ClipProviderInner extends Component<IPropsInner> {
   public componentDidMount() {
-    // this.props.subscribePostRemoved();
-    // this.props.subscribePostCommentCountChanged();
-    // this.props.subscribePostReactionChanged();
+    this.props.clipUpdated();
   }
 
   public render() {
@@ -77,107 +68,47 @@ interface IProps {
   children: any;
 }
 
-const ClipProvider: FC<IProps> = ({ children, where, noRealtime }) => {
-  return (
-    <Query query={GET_CLIP} variables={{ where }}>
-      {({ subscribeToMore, loading, error, data }) => {
-        if (loading) {
-          return null;
-        }
+export const ClipProvider: FC<IProps> = ({ children, where, noRealtime }) => (
+  <Query query={GET_CLIP} variables={{ where }}>
+    {({ subscribeToMore, loading, error, data }) => {
+      if (loading || error || !data || !data.clip) {
+        return children({ clip: null });
+      }
 
-        if (error) {
-          return null;
-        }
+      const clip = data.clip;
 
-        if (noRealtime) {
-          return children({
-            clip: data.clip
-          });
-        }
+      if (noRealtime) {
+        return children({ clip });
+      }
 
-        return (
-          <ClipProviderInner
-            clip={data.clip}
-            // subscribePostRemoved={() => {
-            //   subscribeToMore({
-            //     document: POST_REMOVED,
-            //     variables: { id },
-            //     updateQuery: (prev, { subscriptionData }) => {
-            //       if (!subscriptionData.data) {
-            //         return prev;
-            //       }
+      return (
+        <ClipProviderInner
+          clip={clip}
+          clipUpdated={() => {
+            subscribeToMore({
+              document: CLIP_UPDATED,
+              variables: { where: { node: where } },
+              updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) {
+                  return prev;
+                }
 
-            //       return {
-            //         ...prev,
-            //         post: null
-            //       };
-            //     }
-            //   });
-            // }}
-            // subscribePostCommentCountChanged={() => {
-            //   subscribeToMore({
-            //     document: POST_COMMENT_COUNT_CHANGED,
-            //     variables: { id },
-            //     updateQuery: (prev, { subscriptionData }) => {
-            //       if (!subscriptionData.data) {
-            //         return prev;
-            //       }
+                return {
+                  ...prev,
+                  clip: {
+                    ...prev.clip,
+                    ...subscriptionData.data.clip.node
+                  }
+                };
+              }
+            });
+          }}
+        >
+          {children}
+        </ClipProviderInner>
+      );
+    }}
+  </Query>
+);
 
-            //       const commentsCount =
-            //         subscriptionData.data.postCommentCountChanged;
-
-            //       return {
-            //         ...prev,
-            //         post: {
-            //           ...prev.post,
-            //           commentsCount
-            //         }
-            //       };
-            //     }
-            //   });
-            // }}
-            // subscribePostReactionChanged={() => {
-            //   subscribeToMore({
-            //     document: POST_REACTION_CHANGED,
-            //     variables: { id },
-            //     updateQuery: (prev, { subscriptionData }) => {
-            //       if (!subscriptionData.data) {
-            //         return prev;
-            //       }
-
-            //       // TODO: rethink
-            //       const token = getAccessToken();
-            //       let currentId = null;
-
-            //       if (token) {
-            //         currentId = JSON.parse(atob(token.split('.')[1])).userId;
-            //       }
-
-            //       const reactionData =
-            //         subscriptionData.data.postReactionChanged;
-
-            //       return {
-            //         ...prev,
-            //         post: {
-            //           ...prev.post,
-            //           reaction:
-            //             reactionData.userId === currentId
-            //               ? reactionData.reaction
-            //               : prev.post.reaction,
-            //           likes: reactionData.likes,
-            //           dislikes: reactionData.dislikes,
-            //           rating: reactionData.rating
-            //         }
-            //       };
-            //     }
-            //   });
-            // }}
-          >
-            {children}
-          </ClipProviderInner>
-        );
-      }}
-    </Query>
-  );
-};
 export default ClipProvider;
