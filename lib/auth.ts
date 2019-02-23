5e2f5210f6494e2a9fd887d091acbf7b45783936
Sygeman <sygeman@gmail.com>
2019-02-23 17:47:30 +0300

    skip ssr auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,136 +1,39 @@
 import * as Cookies from 'js-cookie';
 import jwt from 'jsonwebtoken';
-import { destroyCookie, parseCookies, setCookie } from 'nookies';
 import config from '../config';
+import { gqlQuery } from '../utils/gqlQuery';
+import { promiseTimeout } from '../utils/promiseTimeout';
 
-export const connect = (serviceName: string) => {
-  location.href = `${config.apiUrl}auth/connect/${serviceName}`;
+export const getAccessToken = () => Cookies.get('accessToken') || '';
+export const getRefreshToken = () => Cookies.get('refreshToken') || '';
+
+export const getTokens = () => ({
+  accessToken: getAccessToken(),
+  refreshToken: getRefreshToken()
+});
+
+export const setAccessToken = (accessToken: string) => {
+  Cookies.set('accessToken', accessToken, config.cookieOptions);
 };
 
-export const disconnect = (serviceName: string) => {
-  location.href = `${config.apiUrl}disconnect/${serviceName}`;
+export const setRefreshToken = (refreshToken: string) => {
+  Cookies.set('refreshToken', refreshToken, config.cookieOptions);
 };
 
-export const getAccessToken = (ctx = null) => {
-  if (ctx) {
-    if (typeof ctx.ssrAccessTokenHook === 'string') {
-      return ctx.ssrAccessTokenHook;
-    }
-
-    const cookieParsed = parseCookies(ctx);
-    return cookieParsed.accessToken;
-  }
-
-  return Cookies.get('accessToken') || '';
+export const setTokens = (accessToken: string, refreshToken: string) => {
+  setAccessToken(accessToken);
+  setRefreshToken(refreshToken);
 };
 
-export const getRefreshToken = (ctx = null) => {
-  if (ctx) {
-    if (typeof ctx.ssrRefreshTokenHook === 'string') {
-      return ctx.ssrRefreshTokenHook;
-    }
-
-    const cookieParsed = parseCookies(ctx);
-    return cookieParsed.refreshToken;
-  }
-
-  return Cookies.get('refreshToken') || '';
-};
-
-export const getTokens = (ctx = null) => {
-  return {
-    accessToken: getAccessToken(ctx),
-    refreshToken: getRefreshToken(ctx)
-  };
-};
-
-export const setAccessToken = (accessToken: string, ctx = null) => {
-  if (ctx) {
-    setCookie(ctx, 'accessToken', accessToken, config.cookieOptions);
-    ctx.ssrAccessTokenHook = accessToken;
-  } else {
-    Cookies.set('accessToken', accessToken, config.cookieOptions);
-  }
-};
-
-export const setRefreshToken = (refreshToken: string, ctx = null) => {
-  if (ctx) {
-    setCookie(ctx, 'refreshToken', refreshToken, config.cookieOptions);
-    ctx.ssrRefreshTokenHook = refreshToken;
-  } else {
-    Cookies.set('refreshToken', refreshToken, config.cookieOptions);
-  }
-};
-
-export const setTokens = (
-  accessToken: string,
-  refreshToken: string,
-  ctx = null
-) => {
-  setAccessToken(accessToken, ctx);
-  setRefreshToken(refreshToken, ctx);
-};
-
-export const removeTokens = (ctx = null) => {
-  // console.log('removeTokens', !!ctx);
-
-  destroyCookie(ctx, 'accessToken', config.cookieOptions);
-  destroyCookie(ctx, 'refreshToken', config.cookieOptions);
-
-  if (ctx) {
-    ctx.ssrAccessTokenHook = '';
-    ctx.ssrRefreshTokenHook = '';
-  } else {
-    Cookies.remove('accessToken', config.cookieOptions);
-    Cookies.remove('refreshToken', config.cookieOptions);
-  }
+export const removeTokens = () => {
+  Cookies.remove('accessToken', config.cookieOptions);
+  Cookies.remove('refreshToken', config.cookieOptions);
 };
 
 export const accessTokenIsValid = token => {
   const expUnix = jwt.decode(token).exp * 1000;
   const nowUnix = new Date().getTime();
   return expUnix - nowUnix > 0;
-};
-
-const REFRESH_QUERY = `
-  mutation refreshTokens($refreshToken: String!) {
-    refreshTokens(refreshToken: $refreshToken) {
-      refreshToken
-      accessToken
-    }
-  }
-`;
-
-const gqlQuery = async (query, variables) => {
-  const res = await fetch(config.gqlUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      query,
-      variables
-    })
-  });
-
-  return res.json();
-};
-
-const refreshQuery = async refreshToken => {
-  // const qId = +new Date();
-  // console.log('start refreshQuery', qId);
-
-  const query = await gqlQuery(REFRESH_QUERY, {
-    refreshToken
-  });
-
-  if (query.errors) {
-    console.error(query.errors);
-  }
-
-  // console.log('end refreshQuery', qId, refreshToken);
-
-  return query.data.refreshTokens;
 };
 
 class TokenRefresh {
@@ -140,21 +43,16 @@ class TokenRefresh {
   private queue = [];
 
   constructor() {
-    setTimeout(() => {
-      this.runFetchInterval();
-    }, 200);
+    setTimeout(() => this.runFetchInterval(), 200);
   }
 
   public async refresh(refreshToken) {
     if (!this.fetching) {
-      // console.log('start refresh', refreshToken);
       this.fetching = true;
       this.refreshToken = refreshToken;
     }
 
-    return new Promise(resolve => {
-      this.queue.push(resolve);
-    });
+    return new Promise(resolve => this.queue.push(resolve));
   }
 
   private async runFetchInterval() {
@@ -162,11 +60,25 @@ class TokenRefresh {
       if (!this.fetchStart && this.fetching && this.refreshToken) {
         this.fetchStart = true;
 
-        const refreshData = await refreshQuery(this.refreshToken);
+        const refreshQuery = await gqlQuery(
+          `
+          mutation refreshTokens($refreshToken: String!) {
+            refreshTokens(refreshToken: $refreshToken) {
+              refreshToken
+              accessToken
+            }
+          }
+      `,
+          { refreshToken: this.refreshToken }
+        );
 
-        this.queue.forEach(resolve => {
-          resolve(refreshData);
-        });
+        if (refreshQuery.errors) {
+          console.error(refreshQuery.errors);
+        }
+
+        const refreshData = refreshQuery.data.refreshTokens;
+
+        this.queue.forEach(resolve => resolve(refreshData));
 
         this.fetchStart = false;
         this.fetching = false;
@@ -178,55 +90,36 @@ class TokenRefresh {
 
 const tokenRefresh = new TokenRefresh();
 
-const refresh = async (refreshToken, ctx) => {
+const refresh = async refreshToken => {
   const refreshData: any = await tokenRefresh.refresh(refreshToken);
 
   if (refreshData) {
-    setTokens(refreshData.accessToken, refreshData.refreshToken, ctx);
-    console.log('tokens refreshed');
+    setTokens(refreshData.accessToken, refreshData.refreshToken);
     return refreshData.accessToken;
   }
 
-  removeTokens(ctx);
+  removeTokens();
 
   return '';
 };
 
-const promiseTimeout = (ms: number, promise) => {
-  // Create a promise that rejects in <ms> milliseconds
-  const timeout = new Promise((resolve, reject) => {
-    const id = setTimeout(() => {
-      clearTimeout(id);
-      reject('Timed out in ' + ms + 'ms.');
-      resolve();
-    }, ms);
-  });
-
-  // Returns a race between our timeout and the passed in promise
-  return Promise.race([promise, timeout]);
-};
-
-export const getAccessTokenAsync = async ctx => {
-  const { accessToken, refreshToken } = getTokens(ctx);
+export const getAccessTokenAsync = async () => {
+  const accessToken = getAccessToken();
 
   if (!accessToken || accessTokenIsValid(accessToken)) {
     return accessToken;
   }
 
+  const refreshToken = getRefreshToken();
+
   if (!refreshToken) {
-    removeTokens(ctx);
+    removeTokens();
     return '';
   }
 
-  // console.log('need refresh token', refreshToken);
-
-  return promiseTimeout(3000, refresh(refreshToken, ctx))
-    .then(newToken => {
-      return newToken;
-    })
-    .catch(() => {
-      console.log('refresh token timeout', refreshToken);
-      removeTokens(ctx);
-      return '';
-    });
+  return promiseTimeout(3000, refresh(refreshToken)).catch(() => {
+    console.error('refresh token timeout', refreshToken);
+    removeTokens();
+    return '';
+  });
 };
